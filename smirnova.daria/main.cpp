b9b0090fee0dd0718728de125d7bba2492253fec
dassrm <daryasm1rnoff@yandex.ru>
2026-03-05 12:45:55 +0300

    unique realization

---
+++ smirnova.daria/main.cpp
@@ -0,0 +1,26 @@
+template<class T>
+T* unique(T* a, size_t s) {
+    if (s <= 1) return a + s;
+    
+    size_t write_pos = 1;
+    
+    for (size_t read_pos = 1; read_pos < s; ++read_pos) {
+        bool is_duplicate = false;
+        
+        for (size_t check_pos = 0; check_pos < write_pos; ++check_pos) {
+            if (a[read_pos] == a[check_pos]) {
+                is_duplicate = true;
+                break;
+            }
+        }
+        
+        if (!is_duplicate) {
+            if (read_pos != write_pos) {
+                a[write_pos] = a[read_pos];
+            }
+            ++write_pos;
+        }
+    }
+    
+    return a + write_pos;
+}
